74751b1fbbfdf1a5c1f1fd292e291d75b95a7cc3
Edwin Córdoba <edwin.cordoba@gmail.com>
2026-07-01 07:25:09 -0500

    actualización bisección

--- raíces_ecuaciones.py
+++ raíces_ecuaciones.py
@@ -3,27 +3,24 @@ import numpy as np
 from mnspy import Biseccion
 
 
-
 class BiseccionAnimacion(MovingCameraScene):
     def construct(self):
         # Definir la función para Bisección: f(x) = 8-4.5(x-\sin(x))
         def f(x):
-            return 8 - 4.5 * (x - np.sin(x))
+            return np.sin(np.cos(np.exp(x)))
 
-        bis = Biseccion(f, 2, 3, 0.001, tipo_error='t')
+        bis = Biseccion(f, -1, 1.5, 0.1, tipo_error='%')
         # Configurar los ejes cartesianos
         axes = Axes(
-            x_range=[1.0, 3, 1],
-            y_range=[-6, 3, 1],
+            x_range=[-1.0, 2, 1],
+            y_range=[-1.0, 1.0, 1],
             axis_config={"include_tip": True}
         )
         axes.to_edge(DOWN, buff=0.5)
         labels = axes.get_axis_labels(x_label="x", y_label="f(x)")
 
-
-
-        graph = axes.plot(f, color=BLUE, x_range=[0, 3])
-        graph_label = axes.get_graph_label(graph, label=r"8-4.5(x-\sin(x))", x_val=0.5, buff=2, direction=UR).scale(0.6)
+        graph = axes.plot(f, color=BLUE, x_range=[-1, 1.5])
+        graph_label = axes.get_graph_label(graph, label=r"\sin(\cos(e^x))", x_val=0.5, buff=2, direction=UR).scale(0.6)
 
         self.play(Create(axes), Write(labels), run_time=3)
         self.play(Create(graph), Write(graph_label))
@@ -43,17 +40,19 @@ class BiseccionAnimacion(MovingCameraScene):
         # Parámetros iniciales de Bisección
         # a, b = bis._rango
         # a, b = 0.5, 3.5
-        escala =1.0
+        escala = 1.0
         for i in range(10):
             # Dibujar el intervalo [a, b]
             a, b = bis._tabla['x_min'][i], bis._tabla['x_max'][i]
-            line_a = axes.get_vertical_line(axes.c2p(a, f(a)), color=YELLOW, stroke_width=2*escala,line_config={"dash_length": 0.05 * escala})
-            #line_a = line_a.to_dashed(dash_length=0.15, spacing=0.1)
-            line_b = axes.get_vertical_line(axes.c2p(b, f(b)), color=YELLOW, stroke_width=2*escala,line_config={"dash_length": 0.05 * escala})
+            line_a = axes.get_vertical_line(axes.c2p(a, f(a)), color=YELLOW, stroke_width=2 * escala,
+                                            line_config={"dash_length": 0.05 * escala})
+            # line_a = line_a.to_dashed(dash_length=0.15, spacing=0.1)
+            line_b = axes.get_vertical_line(axes.c2p(b, f(b)), color=YELLOW, stroke_width=2 * escala,
+                                            line_config={"dash_length": 0.05 * escala})
             dot_a = Dot(axes.c2p(a, 0), color=YELLOW).scale(escala)
             dot_b = Dot(axes.c2p(b, 0), color=YELLOW).scale(escala)
-            label_a = MathTex(f"a_{i}").scale(0.6*escala).next_to(dot_a, DOWN*escala, buff=0.05)
-            label_b = MathTex(f"b_{i}").scale(0.6*escala).next_to(dot_b, DOWN*escala, buff=0.05)
+            label_a = MathTex(f"a_{i}").scale(0.6 * escala).next_to(dot_a, DOWN * escala, buff=0.05)
+            label_b = MathTex(f"b_{i}").scale(0.6 * escala).next_to(dot_b, UP * escala, buff=0.05)
 
             self.play(Create(line_a), Create(line_b), FadeIn(dot_a, dot_b), FadeIn(label_a), FadeIn(label_b))
 
@@ -61,7 +60,7 @@ class BiseccionAnimacion(MovingCameraScene):
             # m = (a + b) / 2
             m = bis._tabla['x'][i]
             dot_m = Dot(axes.c2p(m, 0), color=RED).scale(escala)
-            label_m = MathTex(f"m_{i}").scale(0.6*escala).next_to(dot_m, UP*escala, buff=0.05)
+            label_m = MathTex(f"m_{i}").scale(0.6 * escala).next_to(dot_m, UP * escala, buff=0.05)
 
             # 1. Convertimos los puntos 'a' y 'b' a coordenadas de pantalla
             coord_a = axes.c2p(a, 0)
@@ -76,7 +75,7 @@ class BiseccionAnimacion(MovingCameraScene):
             # Opcional (¡Muy recomendado!):
             # Evitar que la cámara haga un zoom infinito si 'a' y 'b' están casi pegados.
             # Esto establece que el ancho nunca sea menor a 1 unidad.
-            #nuevo_ancho = max(nuevo_ancho, 1.0)
+            # nuevo_ancho = max(nuevo_ancho, 1.0)
 
             # 4. Aplicamos el ancho exacto con set_width
             self.play(
@@ -96,15 +95,28 @@ class BiseccionAnimacion(MovingCameraScene):
             #     b = m
             # else:
             #     a = m
+            if f(a) * f(m) < 0:
+                self.play(dot_b.animate.move_to(axes.c2p(m, 0)), run_time=2)
+                self.play(
+                    FadeOut(line_a), FadeOut(line_b), FadeOut(dot_m), FadeOut(label_a), FadeOut(dot_a), FadeOut(dot_b),
+                    FadeOut(label_b), FadeOut(label_m), run_time=2.5
+                )
+
+            else:
+                self.play(dot_a.animate.move_to(axes.c2p(m, 0)), run_time=2)
+                self.play(
+                    FadeOut(line_a), FadeOut(line_b), FadeOut(dot_m), FadeOut(label_a), FadeOut(dot_a), FadeOut(dot_b),
+                    FadeOut(label_b), FadeOut(label_m), run_time=2.5
+                )
 
             # Limpiar para la siguiente iteración
-            self.play(
-                FadeOut(line_a), FadeOut(line_b), FadeOut(dot_a), 
-                FadeOut(dot_b), FadeOut(dot_m), FadeOut(label_a), 
-                FadeOut(label_b), FadeOut(label_m), run_time=5.5
-            )
+            # self.play(
+            #     FadeOut(line_a), FadeOut(line_b), FadeOut(dot_a),
+            #     FadeOut(dot_b), FadeOut(dot_m), FadeOut(label_a),
+            #     FadeOut(label_b), FadeOut(label_m), run_time=2.5
+            # )
 
-            #escala*=0.65
+            # escala*=0.65
             escala = nuevo_ancho / config.frame_width
 
         self.play(Restore(self.camera.frame), run_time=2)
